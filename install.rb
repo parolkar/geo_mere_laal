e6a17fb84d8099e355182c3622046c68eb78ee5b
Abhishek Parokar <abhishek@parolkar.com>
2009-10-21 15:05:23 +0530

    Changes in install script

--- install.rb
+++ install.rb
@@ -4,9 +4,10 @@
  def welcome_screen
   
   mesg = <<HERE
-Congratulations for setting the plugin! There are few things to remember here...
-
-Enjoy!
+Congratulations for setting up GeoMereLaal plugin!.
+You can find latest information about the plugin by visiting http://wiki.github.com/parolkar/geo_mere_laal
+Enjoy!      
+Author: Abhishek Parolkar (abhishek[at]parolkar.com)
 HERE
 
   puts mesg
@@ -24,7 +25,7 @@ FileUtils.mkdir_p("#{root}/config/initializers") unless File.exists?("#{root}/co
   files_to_be_copied.each {|ftbc|
     FileUtils.cp_r(root+ftbc[:source], root+ftbc[:target]) #:force => true)
     puts "Created : #{ftbc[:target]}"
-    }
+  }
 
 welcome_screen
     
